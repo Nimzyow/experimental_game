95c0198db3bd36491a54df599878dc71eacc147b
Nimzyow <n_soufiani@hotmail.com>
2020-07-06 00:45:56 +0100

    player flips horizontally on left input

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -20,6 +20,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void Run()
@@ -27,6 +28,15 @@ public class Player : MonoBehaviour
         float controlThrow = Input.GetAxis("Horizontal"); ;
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 
 }
